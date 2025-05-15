927cb6f9ffbd539bef632b517e06f80e7bf2497f
neiranie <nurkhairanianneira@gmail.com>
2025-05-15 10:35:00 +0700

    commit 9

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -101,4 +101,17 @@ public:
             current = current->next;
 
             if (current == NULL)
+            {
+                cout << "Record not found" << endl;
+                return;
+            }
+
+            // Step 2: If node is at the beginning
+            if (current == START)
+            {
+                START = current->next; // Step 2a: START = START.next
+                if (START != NULL)
+                    START->prev = NULL; // Step 2b: START.prev = NULL
+            }
+            else
     
