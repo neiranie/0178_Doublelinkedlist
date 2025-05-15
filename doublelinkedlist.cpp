5bc8196e8acbc791935d28a801e3c20eb3bd6a42
neiranie <nurkhairanianneira@gmail.com>
2025-05-15 10:40:58 +0700

    commit 10

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -114,4 +114,22 @@ public:
                     START->prev = NULL; // Step 2b: START.prev = NULL
             }
             else
+            {
+                // Step 3: Link previous node to next of current
+                current->prev->next = current->next;
+
+                // Step 4: If current is not the last node
+                if (current->next != NULL)
+                    current->next->prev = current->prev;
+            }
+
+            // Step 5: Delete the node
+            delete current;
+            cout << "Record with roll number " << rollNo << " deleted" << endl;
+        }
+
+        void traverse()
+        {
+            if (START == NULL)
+        }
     
