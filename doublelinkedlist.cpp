da49e431b781983b1b6306367b59ef87fb6170e3
neiranie <nurkhairanianneira@gmail.com>
2025-05-15 09:49:17 +0700

    commit 2

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -9,3 +9,22 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        string nim;
+        cout << "\nEnter the roll number of the student: ";
+    }
+}
